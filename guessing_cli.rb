232aaf6c82e14015a47f6af180f35ba74d73bad2
Connor Finnegan <connorfinnegan16@gmail.com>
2018-11-08 05:43:08 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,6 @@ require "pry"
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   guess = gets.chomp
-  comp_guess = prng.rand(1..6)
+  comp_guess = Random.rand(1..6)
   binding.pry
 end
